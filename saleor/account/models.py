8d9a3f1d72ebc5673f9bb2c650d0f56d4e1ec6b4
Marcin Gębala <maarcin.gebala@gmail.com>
2019-03-14 11:50:40 +0100

    Add default ordering for Address models

--- saleor/account/models.py
+++ saleor/account/models.py
@@ -33,6 +33,9 @@ class Address(models.Model):
     country_area = models.CharField(max_length=128, blank=True)
     phone = PossiblePhoneNumberField(blank=True, default='')
 
+    class Meta:
+        ordering = ('pk', )
+
     @property
     def full_name(self):
         return '%s %s' % (self.first_name, self.last_name)
@@ -41,16 +44,6 @@ class Address(models.Model):
         if self.company_name:
             return '%s - %s' % (self.company_name, self.full_name)
         return self.full_name
-
-    def __repr__(self):
-        return (
-            'Address(first_name=%r, last_name=%r, company_name=%r, '
-            'street_address_1=%r, street_address_2=%r, city=%r, '
-            'postal_code=%r, country=%r, country_area=%r, phone=%r)' % (
-                self.first_name, self.last_name, self.company_name,
-                self.street_address_1, self.street_address_2, self.city,
-                self.postal_code, self.country, self.country_area,
-                self.phone))
 
     def __eq__(self, other):
         return self.as_data() == other.as_data()
